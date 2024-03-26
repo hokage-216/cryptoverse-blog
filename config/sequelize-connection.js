d9f6a0f78d87fffe9b426e69be7f43bffca2c05b
LeVente Berry <berrylevente@gmail.com>
2024-03-25 23:29:42 -0400

    sequelize connection code

---
+++ config/sequelize-connection.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.URI);
+}
+
+module.exports = sequelize;
