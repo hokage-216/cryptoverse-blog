129272b8683fc30ac7a4bb056fb3a88f8ac1999d
LeVente Berry <berrylevente@gmail.com>
2024-04-04 15:53:21 -0400

    working express route setup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+// const { User } = require('../models');
+// const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     try {
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
 const sequelize = require('./config/sequelize-connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
-const sess = {
-  secret: 'secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const sess = {
+//   secret: 'secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -34,5 +34,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 });
